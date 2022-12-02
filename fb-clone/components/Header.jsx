fee47af4097fa085bf97a105e27b2fa3e359529e
maicol <maykolandres4@gmail.com>
2022-12-02 06:58:49 -0500

    added header

---
+++ fb-clone/components/Header.jsx
@@ -0,0 +1,100 @@
+import Image from 'next/image'
+import React from 'react'
+import {
+    BellIcon,
+    SupportIcon,
+    ChevronDownIcon,
+    HomeIcon,
+    ViewGridIcon,
+} from "@heroicons/react/solid";
+
+import {
+    PlayIcon,
+    SearchIcon,
+    UserGroupIcon,
+    ShoppingCartIcon,
+} from "@heroicons/react/outline";
+import HeaderIcon from './HeaderIcon';
+import { BsMessenger } from 'react-icons/bs'
+
+
+
+
+
+
+
+
+const Header = () => {
+
+   
+
+
+  return (
+    <div className='sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 lg:pt-7'>
+
+        {/**left */}
+         <div 
+         className='flex items-center'
+         >
+            <Image
+            src="/logo.png"
+            width={40}
+            height={40}
+            layout="fixed"
+            />
+    
+         <div
+          className='flex ml-4 items-center p-2 text-xl'
+          >
+            <SearchIcon
+             className='h-6 text-gray-400'
+              />
+            <input
+             className='hidden md:inline-flex ml-3 bg-transparent items-center outline-none placeholder-gray-400 flex-shrink'
+              type="text"
+              placeholder='Search . . .'
+              />
+         </div>
+         </div> 
+        {/**center */}
+         <div className='flex justify-center flex-grow'>
+            <div className='flex space-x-6 md:space-x-2'>
+                <HeaderIcon active Icon={HomeIcon} />
+                <HeaderIcon Icon={ViewGridIcon}/>
+                <HeaderIcon Icon={PlayIcon}/>
+                <HeaderIcon Icon={ShoppingCartIcon} />
+                <HeaderIcon Icon={UserGroupIcon} />
+            </div>
+         </div>
+        {/**right */}
+        <div className='flex items-center sm:space-x-3 justify-end'>
+          
+           <SupportIcon className='icon'/>
+            <BsMessenger className='icon-messanger'/>
+           <div>
+           <div className='hidden xl:block h-2.5 w-2.5 absolute bg-red-500 rounded-full ml-6 mt-1'/>
+            <BellIcon className='icon'/>
+           </div>
+
+            {/**profile pic */}
+       <div className='flex items-center border-l p-2'>
+       <Image
+            onClick={() => signOut()}
+         
+            height="40"
+            width="40"
+            className="rounded-full cursor-pointer mx-2"
+            layout="fixed"
+            
+            />
+
+            <p className='hidden lg:block whitespace-nowrap font-semibold pr-3'>maicol</p>
+       </div>
+
+            <ChevronDownIcon className='icon'/>
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ fb-clone/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCDK2mSVPiH545ejPnfabWUFOyk02ykEoI",
+  authDomain: "fb-clone-5c4be.firebaseapp.com",
+  projectId: "fb-clone-5c4be",
+  storageBucket: "fb-clone-5c4be.appspot.com",
+  messagingSenderId: "933349211063",
+  appId: "1:933349211063:web:f1f7603c3e4f1d74f58632"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
