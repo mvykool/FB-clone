924b2cf632fe7626f05401133d2cf9e0ec0b5f30
maicol <maykolandres4@gmail.com>
2022-12-02 07:30:24 -0500

    changed the tailwind config

--- fb-clone/tailwind.config.js
+++ fb-clone/tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      
+        colors: {
+          'custom-bg':'#EEF0F4',
+        }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar-hide')
+  ],
 }
