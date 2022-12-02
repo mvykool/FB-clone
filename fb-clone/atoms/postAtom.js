1473fb6d1d2edc5480a4654038ec8515887e8363
maicol <maykolandres4@gmail.com>
2022-12-02 09:41:06 -0500

    ui done

---
+++ fb-clone/atoms/postAtom.js
@@ -0,0 +1,6 @@
+import { atom } from 'recoil';
+
+export const postState = atom ({
+    key: "modalState",
+    default: false,
+});
--- fb-clone/components/Posts.jsx
+++ fb-clone/components/Posts.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
-import Post from './Post';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import React, {useState, useEffect} from 'react'
+import { db } from '../firebase';
+import Post from './Post'
 
-const Posts = ({ posts }) => {
+const Posts = () => {
 
-    
+
+// state 
+
+const [posts, setPosts ] = useState([]);
+
+//use effect for the snapshots
+
+ useEffect(() => {
+   const unsubscribe = onSnapshot(query(collection(db, "posts"),orderBy("timestamp", "desc")), snapshot => {
+        setPosts(snapshot.docs);
+   });
+
+   return unsubscribe;
+ }, [db])
+
+
   return (
     <div>
-    
-            <Post
-        
-            />
-    
-       
-         
-        
+        {/**post */}
+        {posts.map((post) => (
+           <Post 
+           key={post.id}
+           id={post.id}
+           img={post.data().image}
+           caption={post.data().caption}
+           />
+        ))}
     </div>
   )
 }
